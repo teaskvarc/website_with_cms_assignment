bc04938d1fce22a3dcfc41177906dfa54746556b
teaskv <teaskvarc@gmail.com>
2016-08-21 13:47:59 +0200

    (server is set up)

---
+++ api/config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+
+    PORT: 3010
+
+
+};
--- api/server.js
+++ api/server.js
@@ -1,7 +1,21 @@
-//funkcija, ki jo poklicemo iz run.js
+const express           = require('express');
+const server            = express();
+const bodyParser        = require('body-parser');
+const cors              = require('cors');
+const PORT = require('./config').PORT;
 
 exports.init = ()=>{
 
-    console.log('Start server');
+    //vsak req, ki bo prisel na server, bo sel cez te tri middleware
+    //sele potem gre req na routes, ki smo jih spisali
+    server.use(bodyParser.json());
+    server.use(bodyParser.urlencoded({ extended: true}));
+    server.use(cors());
+
+    server.listen(PORT, ()=>{
+
+        console.log('Server started');
+
+    });
 
 };
