8ce8eaa95aef1b970a8ddb83873c6226250791ec
teaskv <teaskvarc@gmail.com>
2016-09-20 23:05:00 +0200

    added login.html, login.js and login-service

--- app/partial/login/login.js
+++ app/partial/login/login.js
@@ -1,4 +1,24 @@
-angular.module('app').controller('LoginCtrl',function($scope){
+angular.module('app').controller('LoginCtrl',function(
+    $scope,
+    loginService,
+    $state,
+    NET
+){
 
+    $scope.NET = NET;
+    
+    $scope.user = {
 
+        email:null,
+        password:null
+
+    };
+
+    $scope.loginClick = function () {
+
+      loginService.login($scope.user)
+          .then(function () {
+              $state.go('app.projects');
+          });
+    };
 });
--- app/service/login-service.js
+++ app/service/login-service.js
@@ -1,6 +1,108 @@
-angular.module('app').factory('loginService',function() {
+angular.module('app').factory('loginService',function(
 
-	var loginService = {};
+    $timeout,
+    $rootscope,
+    $http,
+    dataService,
+    $localForage,
+    $state,
+    NET
+
+) {
+
+	var loginService = {
+
+        isLoggedIn: function () {
+
+            return loginService.getToken()
+                .then(function () {
+                    return $http.post(NET.API_URL + '/api/account/checkLogin')
+                        .then(function (res) {
+                            console.log('checklogin');
+
+                            dataService.model.loggedIn = true;
+                            dataService.model.userPermissions = res.data;
+                            $rootscope.isLoggedIn = true;
+                            return res.data;
+                        })
+                        .catch(function (err) {
+                            console.log('Error: ',err);
+                            $state.go('login', null, {reload:true});
+                        });
+                });
+
+        },
+
+        login: function (userData) {
+
+            return $http.post(NET.API_URL + '/api/account/login', userData)
+                .then(function (res) {
+
+                    $rootscope.isLoggedIn = true;
+                    return loginService.setToken(res.data);
+
+                })
+                .catch(function (err) {
+
+                    console.log(err);
+                    return err;
+                });
+        },
+
+        setToken: function (userData) {
+
+            return $localForage.setItem('user', userData)
+                .then(function (userData) {
+
+                    dataService.model.user = userData;
+                    return userData;
+                });
+        },
+
+        getToken: function () {
+
+            return $localForage.getItem('user')
+                .then(function (userData) {
+                    if(userData){
+                        dataService.model.user = userData;
+                        return userData;
+                    }else{
+                        return null;
+                    }
+                });
+        },
+
+        logout: function () {
+
+            return $http.get(NET.API_URL + '/api/account/logout')
+                .then(function () {
+                    return $localForage.clear();
+                });
+
+
+        },
+
+        forgotten: function (email) {
+
+            return $http.post(NET.API_URL + '/api/account/forgotten', {email:email})
+                .then(function (res) {
+
+                    return res.data;
+                });
+
+
+        },
+
+        confirmReset: function (key, password) {
+
+            return $http.post(NET.API_URL + '/api/account/reset-password', {key:key, password:password})
+                .then(function (res) {
+                    return res.data;
+
+                });
+
+        }
+    };
 
 	return loginService;
 });
